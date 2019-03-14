e69f9536530c022cd04f7254498197f5c7d9a62f
Andrews Frempong <andrews.frempong@padoq.com>
2019-03-14 18:31:38 +0000

    implemented google maps[todo: caching]

--- src/components/rental/RentalDetail.js
+++ src/components/rental/RentalDetail.js
@@ -7,9 +7,27 @@ import { getRental } from "../../actions/rentalsActions";
 
 class RentalDetail extends Component {
 
-    componentDidMount () {
+     componentDidMount () {
         let rentalId = this.props.match.params.id;
-        this.props.dispatch(getRental(rentalId));
+        this.props.getRental(rentalId);
+
+        this.renderMap();
+    };
+
+    initMap = () => {
+        let map = new window.google.maps.Map(document.getElementById('map'), {
+            center: {lat: 41.890251, lng: 12.492373},
+            zoom: 8
+        });
+        // The marker, positioned at Uluru
+        // var marker = new window.google.maps.Marker({position: uluru, map: map});
+
+    };
+
+    renderMap = () => {
+
+        loadScript( "https://maps.googleapis.com/maps/api/js?key=AIzaSyDGQFOv8Dnc8uY7-RGMAf3P4AZk6rBbZes&callback=initMap");
+        window.initMap = this.initMap;
     };
 
     render () {
@@ -19,7 +37,7 @@ class RentalDetail extends Component {
         console.log(this.props);
         return (
 
-            <div id="rental-detail">
+            <div className="container-fluid">
 
                 {/*fetching rental*/}
                 {
@@ -29,12 +47,69 @@ class RentalDetail extends Component {
                 {/* /fetching rental*/}
 
 
-                {/*finished fetching rental*/}
-                {
-                    !isFetchingRental &&
-                    <h1>Rental Detail: {selectedRental.title}</h1>
-                }
-                {/* /finished fetching rental*/}
+                {/*/!*finished fetching rental*!/*/}
+                {/*{*/}
+                    {/*!isFetchingRental &&*/}
+                    {/*<h1>Rental Detail: {selectedRental.title}</h1>*/}
+                {/*}*/}
+                {/*/!* /finished fetching rental*!/*/}
+
+
+                <section id='rentalDetails'>
+                    <div className='upper-section'>
+                        <div className='row'>
+                            <div className='col-md-6 rental-image-container'>
+                                <img src={selectedRental.image} alt='' />
+                            </div>
+                            <div className='col-md-6'>
+
+
+                                <div id="map">
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='details-section'>
+                        <div className='row'>
+                            <div className='col-md-8'>
+                                <div className='rental'>
+                                    <h2 className={`rental-type ${selectedRental.category}`}>{selectedRental.shared} {selectedRental.category}</h2>
+                                    <h1 className='rental-title'>{selectedRental.title}</h1>
+                                    <h2 className='rental-city'>{selectedRental.city}</h2>
+                                    <div className='rental-room-info'>
+                                        <span><i className='fa fa-building'></i>{selectedRental.bedrooms} bedrooms</span>
+                                        <span><i className='fa fa-user'></i> {selectedRental.bedrooms + 4} guests</span>
+                                        <span><i className='fa fa-bed'></i> {selectedRental.bedrooms + 2} beds</span>
+                                    </div>
+                                    <p className='rental-description'>
+                                        {selectedRental.description}
+                                    </p>
+                                    <hr></hr>
+                                    <div className='rental-assets'>
+                                        <h3 className='title'>Assets</h3>
+                                        <div className='row'>
+                                            <div className='col-md-6'>
+                                                <span><i className='fa fa-asterisk'></i> Cooling</span>
+                                                <span><i className='fa fa-thermometer'></i> Heating</span>
+                                                <span><i className='fa fa-location-arrow'></i> Iron</span>
+                                            </div>
+                                            <div className='col-md-6'>
+                                                <span><i className='fa fa-desktop'></i> Working area</span>
+                                                <span><i className='fa fa-cube'></i> Washing machine</span>
+                                                <span><i className='fa fa-cube'></i> Dishwasher</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-md-4'> BOOKING</div>
+                        </div>
+                    </div>
+                </section>
+
 
             </div>
 
@@ -44,7 +119,26 @@ class RentalDetail extends Component {
 
 }
 
-//todo: show selected rental
+//function for google map
+
+function loadScript (url) {
+    const firstScriptTag = window.document.getElementsByTagName("script")[0];
+
+    //creating script for map
+
+    {/*<script async defer*/}
+    {/*src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDGQFOv8Dnc8uY7-RGMAf3P4AZk6rBbZes&callback=initMap">*/}
+    {/*</script>*/}
+
+    let script = window.document.createElement("script");
+    script.async = true;
+    script.defer = true;
+    script.src = url;
+
+    firstScriptTag.parentNode.insertBefore(script, firstScriptTag);
+
+}
+
 
 const mapStateToProps = (state) => {
 
@@ -55,10 +149,12 @@ const mapStateToProps = (state) => {
 
 };
 
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//
-//     }
-// };
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getRental: (rentalId) => {
+            dispatch(getRental(rentalId));
+        }
+    }
+};
 
-export default connect(mapStateToProps)(RentalDetail);
+export default connect(mapStateToProps, mapDispatchToProps)(RentalDetail);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.scss';
 import App from './App';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 //import * as serviceWorker from './serviceWorker';
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+
 import { Provider } from "react-redux";
 import { configureStore } from "./store/configureStore";
 
